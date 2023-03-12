9ce6ad0b7e15fbde1f7e767b6961e0f4c7c4adbe
Yohan Sandun Kumaranayake <yohan.sandunk@gmail.com>
2023-03-12 15:38:57 +0530

    Updated

--- src/date-format.js
+++ src/date-format.js
@@ -21,10 +21,52 @@ const tokens = {
     dddd : date => weekdays[date.getDay()],
     f: date => parseInt(date.getMilliseconds() / 100).toString(),
     ff: date => fill(parseInt(date.getMilliseconds() / 10), 2),
-    
+    fff: date => fill(date.getMilliseconds(), 3),
+//  ffff: is not possible in js
+//  fffff: is not possible in js
+//  ffffff: is not possible in js
+    F: date => date.getMilliseconds() > 0 ? parseInt(date.getMilliseconds() / 100).toString() : '',
+    FF: date => date.getMilliseconds() > 0 ? fill(parseInt(date.getMilliseconds() / 10), 2) : '',
+    FFF: date => date.getMilliseconds() > 0 ? fill(date.getMilliseconds(), 3) : '',
+//  FFFF: is not possible in js
+//  FFFFF: is not possible in js
+//  FFFFFF: is not possible in js
+//  g: era
+//  gg: era (same as g)
+    h: date => (date.getHours() % 12 || 12).toString(),
+    hh: date => fill(date.getHours() % 12 || 12, 2),
+    H: date => date.getHours().toString(),
+    HH: date => fill(date.getHours(), 2),
+//  K: time zone info
+    m: date => date.getMinutes().toString(),
+    mm: date => fill(date.getMinutes(), 2),
+    M: date => (date.getMonth()+1).toString(),
+    MM: date => fill(date.getMonth()+1, 2),
+    MMM: date => shortMonths[date.getMonth()],
+    MMMM: date => months[date.getMonth()],
+    s: date => date.getSeconds().toString(),
+    ss: date => fill(date.getSeconds(), 2),
+    t: date => date.getHours() >= 12 ? 'A' : 'P',
+    tt: date => date.getHours() >= 12 ? 'AM' : 'PM',
+    y: date => parseInt(date.getFullYear().toString().substr(2, 2)).toString(),
+    yy: date => date.getFullYear().toString().substr(2, 2),
+    yyy: date => fill(date.getFullYear(), 3),
+    yyyy: date => date.getFullYear().toString(),
+    yyyyy: date => fill(date.getFullYear(), 5),
+    z: date => date.getTimezoneOffset() > 0 ? '+' + parseInt(date.getTimezoneOffset() / 60).toString() : parseInt(date.getTimezoneOffset() / 60).toString(),
+    zz: date => date.getTimezoneOffset() > 0 ? '+' + fill(parseInt(date.getTimezoneOffset() / 60), 2) : '-' + fill(parseInt(date.getTimezoneOffset()*-1 / 60), 2),
+    zzz: date => {
+        let h = parseInt(date.getTimezoneOffset() / 60);
+        let m = date.getTimezoneOffset() - (h * 60);
+        if (date.getTimezoneOffset() > 0) {
+            return '+' + fill(h, 2) + ':' + fill(m, 2);
+        } else {
+            return '-' + fill(h*-1, 2) + ':' + fill(m*-1, 2);
+        }
+    }
 };
 
-const getTokens = (date, format) => {
+const formatDate = (date, format) => {
     let formatted = format;
     let index = 0;
 
@@ -60,10 +102,29 @@ const format = (date, format) => {
     }
 
     if (format.length === 1) {
-        return 'TODO';
+        //TODO r, s, u
+        if (format[0] == 'y') {
+            return formatDate(date, 'yyyy MMMM');
+        } else if (format[0] == 'd') {
+            return formatDate(date, 'MM/dd/yyyy');
+        } else if (format[0] == 'f') {
+            return formatDate(date, 'dddd, dd MMMM yyyy HH:mm');
+        } else if (format[0] == 'g') {
+            return formatDate(date, 'MM/dd/yyyy HH:mm');
+        } else if (format[0] == 'm') {
+            return formatDate(date, 'MMMM dd');
+        } else if (format[0] == 'o') {
+            return formatDate(date, 'yyyy-MM-ddTHH:mm:ss.fff');
+        } else if (format[0] == 't') {
+            return formatDate(date, 'HH:mm');
+        } else {
+            return 'Invalid format';
+        }
     }
 
-    console.log(getTokens(date, format));
+    return formatDate(date, format);
 }
 
-format(new Date(), "dddd-dd ff");
+Date.prototype.format = function (formatString) {
+    return format(this, formatString);
+}
